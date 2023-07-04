8f6db30f9b12d06de78e3279c75c3c13ca70b6f8
Arianderson <santosaldesareanderson@gmail.com>
2023-07-03 22:32:16 -0300

    Project Restaurant
    This project is a system based on php, html and css at the moment still under development, this system the customer is able to place his order at the restaurant.

---
+++ comanda2.php
@@ -0,0 +1,47 @@
+<?php
+//cria valores dos produtos
+$agua = 3;
+$frango = 26;
+$refrigerante = 5;
+$churrasco = 13;
+$saladaFruta = 7;
+//pedido 1
+$atendente1 = $_POST["atendente1"];
+$mesa1 =  $_POST["mesa1"];
+$pedido1 = $_POST["pedido1"];
+//pedido 2
+$atendente2 = $_POST["atendente2"];
+$mesa2 =  $_POST["mesa2"];
+$pedido2 = $_POST["pedido2"];
+//pedido 3
+$atendente3 = $_POST["atendente3"];
+$mesa3 =  $_POST["mesa3"];
+$pedido3 = $_POST["pedido3"];
+//pedido 4
+$atendente4 = $_POST["atendente4"];
+$mesa4 =  $_POST["mesa4"];
+$pedido4 = $_POST["pedido4"];
+//Mostrando as demandas
+$cont = 1;
+while($cont < 5){
+echo "<b>Nome:</b>" .$_POST["atendente$cont"]."<br />";
+echo "<b>Número da Mesa:</b>" .$_POST["mesa$cont"] ."<br />";
+echo "<b>Pedido:</b>" .$_POST["pedido$cont"] ."<br />";
+//condicao da conta
+if ($_POST["pedido$cont"] == "água") {
+    $conta = "<b>Conta = </b>R$ $agua reais<br />";
+}elseif(($_POST["pedido$cont"] == "refrigerante")){
+    $conta = "<b>Conta = </b>R$ $refrigerante reais<br />";
+}elseif(($_POST["pedido$cont"] == "frango")){
+    $conta = "<b>Conta = </b>R$ $frango reais<br />";
+}elseif(($_POST["pedido$cont"] == "churrasco")){
+    $conta = "<b>Conta = </b>R$ $churrasco reais<br />";
+}elseif(($_POST["pedido$cont"] == "saladaFruta")){
+    $conta = "<b>Conta = </b>R$ $saladaFruta reais<br />";
+}else{
+    echo"Erro no calculo, por favor calcular manualmente!!";
+}
+echo $conta ."<hr>";
+$cont++;
+}
+?>
---
+++ contatos.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="contatos.css">
+    <title>Cafetetia - Coffee</title>
+</head>
+<body>
+    <!--background-->
+    <div class="div1">
+        <!--Menu-->
+    <div class="div_menu">
+        <!--Logo-->
+       <div class="div_nomelogo">
+        <img src="./img/logo_nome.jpg" alt="logo_nome" />
+        <div class="divnav">
+        <nav>
+        <ul>
+            <li><a href="index.php">Home</a></li>
+            <li><a href="menu.php">Menu</a></li>
+            <li><a href="pedidos.php">Pedidos</a></li>
+            <li><a href="contatos.php" style="color: rgb(253, 173, 1);">Contats</a></li>
+        </ul>
+        </nav>
+    </div>
+    </div> 
+    </div>
+    <div class="div_main">
+        <!--Div da esquerda-->
+        <div class="div_left">
+        <img src="./img/logo_nome.jpg" alt="Logo Cafeteria" style="padding: 25px 0;">
+        </div>
+        <div class="div_right">
+            <h2>Contatos/Endereço:</h2>
+            <!-- Cria o formulário com quatro inputs de matrícula -->
+            <div class="contatos">
+                <b>TEL: (99) 9999-9999 <br />
+                Vitória (ES), Ilha dos Chavez <br />
+                Rua Basilio Costa Longa <br />
+                Numero 03, em frente ao Predio La Plazan</b>
+                <br />
+                <div style="margin: auto;">
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2224.5413437838165!2d-40.33255719843413!3d-20.33262399999999!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xb83d3dbe9b2b81%3A0x1006d8416318b266!2sLine%20Doceria!5e0!3m2!1spt-BR!2sbr!4v1688329285875!5m2!1spt-BR!2sbr" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+            </div>
+        </div>
+        </div>
+    </div>
+    </div>
+</body>
+</html>
